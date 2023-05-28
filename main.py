555e978f385363f9e5f0964d6e28fb7d72214807
Dmitriy Chernow <dmitriychernow2000@gmail.com>
2023-05-28 18:34:17 +0300

    refactoring added cors checks; update reqs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import asyncio
-
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from api import router as api_router
 from infrastructure.database import BaseModel, engine
@@ -8,8 +7,15 @@ from infrastructure.database import BaseModel, engine
 app = FastAPI()
 app.include_router(api_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 
 async def create_table():
     async with engine.begin() as conn:
         await conn.run_sync(BaseModel.metadata.create_all)
-
